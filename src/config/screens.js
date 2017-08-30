ac28580fe9e252c2fa69a1e630a10906c8ef46b9
Hebert Porto <hebertporto@gmail.com>
2017-08-30 13:55:46 -0300

    register new screens

--- src/config/screens.js
+++ src/config/screens.js
@@ -1,7 +1,11 @@
 import { Navigation } from 'react-native-navigation'
 
 import MainScreen from '../modules/main/containers/MainScreen'
+import NovelScreen from '../modules/novel/containers/NovelScreen'
+import ChaptherScreen from '../modules/novel/containers/ChapterScreen'
 
 export function registerScreens (store, Provider) {
   Navigation.registerComponent('main', () => MainScreen, store, Provider )
+  Navigation.registerComponent('novel', () => NovelScreen, store, Provider )
+  Navigation.registerComponent('chapter', () => ChaptherScreen, store, Provider )
 }
