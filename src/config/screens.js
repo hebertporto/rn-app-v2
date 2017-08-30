1dfe276debb56c64eb71f2dcb10437df103fd518
Hebert Porto <hebertporto@gmail.com>
2017-08-30 14:39:43 -0300

    added new files for module novel

--- src/config/screens.js
+++ src/config/screens.js
@@ -2,10 +2,10 @@ import { Navigation } from 'react-native-navigation'
 
 import MainScreen from '../modules/main/containers/MainScreen'
 import NovelScreen from '../modules/novel/containers/NovelScreen'
-import ChaptherScreen from '../modules/novel/containers/ChapterScreen'
+import ChapterScreen from '../modules/novel/containers/ChapterScreen'
 
 export function registerScreens (store, Provider) {
   Navigation.registerComponent('main', () => MainScreen, store, Provider )
   Navigation.registerComponent('novel', () => NovelScreen, store, Provider )
-  Navigation.registerComponent('chapter', () => ChaptherScreen, store, Provider )
+  Navigation.registerComponent('chapter', () => ChapterScreen, store, Provider )
 }
---
+++ src/modules/novel/containers/NovelScreen.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import { Text, View } from 'react-native';
+
+import { getInfo } from '../actions/index';
+
+import { connect } from 'react-redux';
+
+class NovelScrenn extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text> Novel Screen </Text>
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    info: state.main.info
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getInfo: () => dispatch(getInfo())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NovelScrenn);
