c0dcc5da99feccc07cbdf1e5a9f9cbde610e7c63
Hebert Porto <hebertporto@gmail.com>
2017-09-02 21:27:14 -0300

    mudando fluxo de atividades na home screen

---
+++ src/modules/play/styles/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  listItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 8 * 2,
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(179, 177, 177, 0.6)',
+    backgroundColor: 'white',
+    flex: 1
+  },
+  header: {
+    flex: 1,
+    height: 100,
+    backgroundColor: 'yellow'
+  }
+})
